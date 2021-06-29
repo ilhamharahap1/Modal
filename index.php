2f000b3b8d27d1b726d59c79a09bfae7b98c36bd
ilhamharahap1 <ilhamharahap59@gmail.com>
2021-06-29 09:35:12 +0700

    file index dan css

---
+++ index.php
@@ -0,0 +1,95 @@
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="card">
+  <div class="content">
+    <h4>Modal Standar</h4>
+    <label class="modal-open modal-label" for="modal-open"><p>Lihat</p></label>
+    <input type="radio" name="modal" value="open" id="modal-open" class="modal-radio">
+
+    <div class="modal">
+      <label class="modal-label overlay"><input type="radio" name="modal" value="close" class="modal-radio"/></label>
+      <div class="content">
+        <div class="top">
+          <h2>Heading </h2>
+          <label class="modal-label close-btn">
+            <input type="radio" name="modal" value="close" class="modal-radio"/>
+          </label>
+        </div>
+        
+
+
+			 <div class="card">
+			
+			<h4>Modal Standar</h4>
+			</div>
+    	
+
+
+
+
+
+      </div>
+    </div>
+    
+  </div>
+</div>
+
+<div class="card">
+  <div class="content">
+    <h3>Atas ke Bawah</h3>
+    <label class="modal-open modal-label" for="modal-open2"><p>Lihat</p></label>
+    <input type="radio" name="modal" value="open" id="modal-open2" class="modal-radio" />
+
+    <div class="modal modal2">
+      <label class="modal-label overlay"><input type="radio" name="modal" value="close" class="modal-radio"/></label>
+      <div class="content">
+        <div class="top">
+          <h2>Heading </h2>
+          <label class="modal-label close-btn"><input type="radio" name="modal" value="close" class="modal-radio"/></label>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="card">
+  <div class="content">
+    <h4>Bawah Ke atas</h4>
+    <label class="modal-open modal-label" for="modal-open3"><p>Lihat</p></label>
+    <input type="radio" name="modal" value="open" id="modal-open3" class="modal-radio" />
+    <div class="modal modal3">
+      <label class="modal-label overlay"><input type="radio" name="modal" value="close" class="modal-radio"/></label>
+      <div class="content">
+        <div class="top">
+          <h2>Heading </h2>
+          <label class="modal-label close-btn"><input type="radio" name="modal" value="close" class="modal-radio"/></label>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card">
+  <div class="content">
+    <h4>Efek Putar</h4>
+    <label class="modal-open modal-label" for="modal-open4"><p>Lihat</p></label>
+    <input type="radio" name="modal" value="open" id="modal-open4" class="modal-radio" />
+    
+    <div class="modal modal4">
+      <label class="modal-label overlay"><input type="radio" name="modal" value="close" class="modal-radio"/></label>
+      <div class="content">
+        <div class="top">
+          <h2>Heading </h2>
+          <label class="modal-label close-btn"><input type="radio" name="modal" value="close" class="modal-radio"/></label>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <label class="modal-label action-btn">OK<input type="radio" name="modal" value="close" class="modal-radio"/>
+        </label>
+      </div>
+    </div>
+  </div>
+</div>
